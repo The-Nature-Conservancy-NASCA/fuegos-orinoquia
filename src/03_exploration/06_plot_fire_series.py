dc8effb5312910b9f5c63739f2f2f64fe7007ebc
Marcelo <marcelo.villa.p@gmail.com>
2020-09-30 21:21:58 -0500

    ignore figures folder

---
+++ src/03_exploration/06_plot_fire_series.py
@@ -0,0 +1,50 @@
+# -----------------------------------------------------------------------
+# Author: Marcelo Villa-Piñeros
+#
+# Purpose: Plots the fire time series for each window in a grid with 2
+# subplots (1 column, 2 rows) where the first plot is the daily time
+# series and the second plot is the monthly time series.
+# -----------------------------------------------------------------------
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from src.utils.constants import WINDOWS
+
+
+if __name__ == "__main__":
+
+    # Project's root
+    os.chdir("../..")
+
+    for i, window in enumerate(WINDOWS):
+
+        output_folder = f"figures/{window['name']}"
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        # Create an empty figure with two subplots.
+        fig, ax = plt.subplots(ncols=1, nrows=2, sharex=True, figsize=(14, 8))
+        fig.suptitle(window["name"])
+
+        series_filepath = f"results/xlsx/{window['name']}/fire_series.xlsx"
+        monthly_series = pd.read_excel(series_filepath, sheet_name="Monthly")
+        daily_series = pd.read_excel(series_filepath, sheet_name="Daily")
+
+        # Make sure the time column in the monthly series is interpreted
+        # as datetime. This is not necessary with the daily series as
+        # pandas interprets the time correctly.
+        monthly_series["time"] = pd.to_datetime(monthly_series["time"])
+
+        sns.lineplot(data=monthly_series, x="time", y="area", ax=ax[0], color="gray",
+                     linewidth=0.75)
+        sns.lineplot(data=daily_series, x="time", y="area", ax=ax[1], color="gray",
+                     linewidth=0.75)
+
+        ax[0].set_ylabel("Burned area (ha)")
+        ax[1].set_ylabel("Burned area (ha)")
+
+        save_to = os.path.join(output_folder, "fire_time_series.png")
+        fig.savefig(save_to)
