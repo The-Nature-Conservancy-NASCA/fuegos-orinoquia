173f8487a5dff4687b367459bd066b1904ea3f08
Marcelo <marcelo.villa.p@gmail.com>
2020-10-03 21:08:40 -0500

    added script

---
+++ src/01_download/03_download_wrs2_grid.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------------------
+# Author: Marcelo Villa-Piñeros
+#
+# Purpose: Downloads the Landsat's World Reference System-2 descending
+# (daytime) grid.
+# -----------------------------------------------------------------------
+import os
+
+
+from src.utils.constants import WRS2_DESCENDING_GRID_URL
+from src.utils.functions import download_http_file
+
+
+if __name__ == "__main__":
+
+    # Project's root
+    os.chdir("../..")
+
+    output_folder = "data/shp/misc"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    save_to = os.path.join(output_folder, "wrs2_descending_grid.zip")
+    download_http_file(WRS2_DESCENDING_GRID_URL, save_to)
