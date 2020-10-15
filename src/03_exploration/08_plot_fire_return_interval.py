6715f2fa5734c95d30d91563cdc2bdcc09644576
Marcelo <marcelo.villa.p@gmail.com>
2020-10-15 08:57:06 -0500

    changed distplot for histplot with kde

--- src/03_exploration/08_plot_fire_return_interval.py
+++ src/03_exploration/08_plot_fire_return_interval.py
@@ -17,18 +17,42 @@ if __name__ == "__main__":
     # Project's root
     os.chdir("../..")
 
-    for i, region in enumerate(REGIONS):
+    output_folder = "figures"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-        output_folder = f"figures/{region['name']}"
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+
+    for i, region in enumerate(REGIONS):
 
         ds = gdal.Open(f"data/tif/return_intervals/RI_500m_{region['name']}.tif")
         arr = ds.ReadAsArray()
         fri = arr[arr != NODATA_VALUE]
 
-        fig = sns.displot(x=fri, kde=True, rug=True)
-        plt.axvline(np.median(fri))
+        ax = axs.ravel()[i]
 
-        save_to = os.path.join(output_folder, "return_interval_distribution.png")
-        fig.savefig(save_to)
+        sns.histplot(
+            x=fri,
+            stat="probability",
+            ax=ax,
+            kde=True,
+            fill=False,
+            discrete=True,
+            multiple="fill",
+            color="#263238",
+            line_kws=dict(linewidth=1)
+        )
+        ax.axvline(np.median(fri), color="#263238", linestyle="--", linewidth=0.8)
+
+        ax.xaxis.set_tick_params(which="both", labelbottom=True)
+        ax.yaxis.set_tick_params(which="both", labelleft=True)
+
+        ax.set_xticks(np.arange(1, fri.max() + 1, 2))
+
+        ax.yaxis.label.set_visible(False)
+        ax.tick_params(labelsize=8)
+
+        ax.set_title(region["name"].upper(), fontsize=8)
+
+    save_to = os.path.join(output_folder, "return_interval_distribution.pdf")
+    fig.savefig(save_to)
