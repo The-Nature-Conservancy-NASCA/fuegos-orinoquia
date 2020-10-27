5137d6fc9b7c24ee1c52267fc116493a2595b4c4
Marcelo <marcelo.villa.p@gmail.com>
2020-10-26 21:44:50 -0500

    new script

---
+++ src/02_wrangling/02_preprocess_landcover.py
@@ -0,0 +1,48 @@
+# -----------------------------------------------------------------------
+# Author: Marcelo Villa-Piñeros
+#
+# Purpose:
+# -----------------------------------------------------------------------
+import glob
+import os
+
+import rioxarray
+import xarray as xr
+from osgeo import gdal
+
+from src.utils.constants import REGIONS
+
+
+if __name__ == "__main__":
+
+    # Project's root
+    os.chdir("../..")
+
+    landcover_filenames = sorted(glob.glob("data/tif/landcover/original/*.tif"))
+
+    for landcover_fn in landcover_filenames:
+        for region in REGIONS:
+
+            output_folder = f"data/tif/landcover/{region['name']}"
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+
+            # Open clipped burned area product to later obtain extent
+            # and resolution from.
+            burned_area_fn = f"data/nc/MODIS/MCD64A1/{region['name']}/MCD64A1_500m.nc"
+            ds = xr.open_dataset(burned_area_fn)
+
+            options = gdal.WarpOptions(
+                format="GTiff",
+                outputBounds=ds.rio.bounds(),
+                xRes=ds.rio.resolution()[0],
+                yRes=abs(ds.rio.resolution()[1]),
+                creationOptions=["COMPRESS=LZW"],
+                resampleAlg="mode",
+                outputType=gdal.GDT_Byte,
+                cutlineDSName=region["path"]
+            )
+
+            basename = os.path.basename(landcover_fn)
+            output_fn = os.path.join(output_folder, basename)
+            gdal.Warp(output_fn, landcover_fn, options=options)
