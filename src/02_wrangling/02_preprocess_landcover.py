6b145b996afb6a4cd8d5924318a05bfad1196b5d
Marcelo <marcelo.villa.p@gmail.com>
2020-10-26 21:48:50 -0500

    changed loop order

--- src/02_wrangling/02_preprocess_landcover.py
+++ src/02_wrangling/02_preprocess_landcover.py
@@ -20,29 +20,29 @@ if __name__ == "__main__":
 
     landcover_filenames = sorted(glob.glob("data/tif/landcover/original/*.tif"))
 
-    for landcover_fn in landcover_filenames:
-        for region in REGIONS:
+    for region in REGIONS:
 
-            output_folder = f"data/tif/landcover/{region['name']}"
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder)
+        output_folder = f"data/tif/landcover/{region['name']}"
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-            # Open clipped burned area product to later obtain extent
-            # and resolution from.
-            burned_area_fn = f"data/nc/MODIS/MCD64A1/{region['name']}/MCD64A1_500m.nc"
-            ds = xr.open_dataset(burned_area_fn)
+        # Open clipped burned area product to later obtain extent
+        # and resolution from.
+        burned_area_fn = f"data/nc/MODIS/MCD64A1/{region['name']}/MCD64A1_500m.nc"
+        ds = xr.open_dataset(burned_area_fn)
 
-            options = gdal.WarpOptions(
-                format="GTiff",
-                outputBounds=ds.rio.bounds(),
-                xRes=ds.rio.resolution()[0],
-                yRes=abs(ds.rio.resolution()[1]),
-                creationOptions=["COMPRESS=LZW"],
-                resampleAlg="mode",
-                outputType=gdal.GDT_Byte,
-                cutlineDSName=region["path"]
-            )
+        options = gdal.WarpOptions(
+            format="GTiff",
+            outputBounds=ds.rio.bounds(),
+            xRes=ds.rio.resolution()[0],
+            yRes=abs(ds.rio.resolution()[1]),
+            creationOptions=["COMPRESS=LZW"],
+            resampleAlg="mode",
+            outputType=gdal.GDT_Byte,
+            cutlineDSName=region["path"]
+        )
 
+        for landcover_fn in landcover_filenames:
             basename = os.path.basename(landcover_fn)
             output_fn = os.path.join(output_folder, basename)
             gdal.Warp(output_fn, landcover_fn, options=options)
