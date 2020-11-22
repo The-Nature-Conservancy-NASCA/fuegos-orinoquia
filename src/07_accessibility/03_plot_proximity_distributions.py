dab6b1314b6c154d7dab897f7ef37e24b43b46eb
Marcelo <marcelo.villa.p@gmail.com>
2020-11-22 00:30:25 -0500

    plot all regions on the same figure

--- src/07_accessibility/03_plot_proximity_distributions.py
+++ src/07_accessibility/03_plot_proximity_distributions.py
@@ -5,11 +5,13 @@
 # -----------------------------------------------------------------------
 import os
 
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
+import seaborn as sns
 import xarray as xr
 from osgeo import gdal
-import seaborn as sns
 
 from src.utils.constants import (
     REGIONS,
@@ -22,13 +24,18 @@ if __name__ == "__main__":
 
     os.chdir("../..")
 
-    for region in REGIONS:
+    output_folder = "figures"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-        output_folder = f"figures/{region.get('name')}"
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+    fig = plt.figure(figsize=(11.69, 10.33))
+    outer = gridspec.GridSpec(2, 2, wspace=0.2, hspace=0.2)
 
-        fig, axs = plt.subplots(2, 2, figsize=(11.69, 5.15))
+    for i, region in enumerate(REGIONS):
+
+        inner = gridspec.GridSpecFromSubplotSpec(
+            4, 1, subplot_spec=outer[i], wspace=0.1, hspace=0.1
+        )
 
         fn = f"data/nc/MODIS/MCD64A1/{region.get('name')}/MCD64A1_500m.nc"
         da = xr.open_dataset(fn, mask_and_scale=False)["Burn_Date"]
@@ -38,11 +45,11 @@ if __name__ == "__main__":
         feature_names = [item.get("name") for item in ACCESSIBILITY_FEATURES]
         feature_names.append("combined")
 
-        for i, name in enumerate(feature_names):
+        for j, name in enumerate(feature_names):
 
             proximity_fn = f"data/tif/proximity/{region.get('name')}/{name}_proximity.tif"
 
-            ax = axs.ravel()[i]
+            ax = plt.Subplot(fig, inner[j])
 
             ds = gdal.Open(proximity_fn)
             arr = ds.ReadAsArray()
@@ -60,10 +67,16 @@ if __name__ == "__main__":
             ax.axvline(np.median(burn_proximity), **kwargs)
             ax.axvline(np.percentile(burn_proximity, 95), **kwargs)
 
-            ax.set_title(name.upper(), fontsize=8)
             ax.yaxis.label.set_visible(False)
             ax.tick_params(labelsize=8)
 
-        save_to = os.path.join(output_folder, "proximity_distributions.pdf")
-        plt.subplots_adjust(hspace=0.5)
-        fig.savefig(save_to, bbox_inches="tight")
+            ax.text(0.85, 0.75, name.upper(), transform=ax.transAxes, fontsize=8)
+
+            if j == 0:
+                ax.set_title(region.get("name").upper(), fontsize=8)
+
+            fig.add_subplot(ax)
+
+    save_to = os.path.join(output_folder, "proximity_distributions.pdf")
+    plt.subplots_adjust(hspace=0.5)
+    fig.savefig(save_to, bbox_inches="tight")
