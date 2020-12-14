a483a7d8e8f2999e59ae13873e4cb7780819dc8d
Marcelo <marcelo.villa.p@gmail.com>
2020-12-14 15:15:15 -0500

    Changed output DataFrame structure.

--- src/05_landcover/01_compute_proportions_and_return_intervals.py
+++ src/05_landcover/01_compute_proportions_and_return_intervals.py
@@ -9,7 +9,12 @@ import pandas as pd
 import xarray as xr
 from osgeo import gdal
 
-from src.utils.constants import REGIONS, LANDCOVER_MAP, LANDCOVER_PADDINGS
+from src.utils.constants import (
+    REGIONS,
+    LANDCOVER_MAP,
+    LANDCOVER_PERIODS,
+    LANDCOVER_PADDING
+)
 
 
 if __name__ == "__main__":
@@ -19,24 +24,27 @@ if __name__ == "__main__":
 
     for region in REGIONS:
 
-        df_proportions = pd.DataFrame(columns=["landcover"])
-        df_proportions["landcover"] = LANDCOVER_MAP.values()
-        df_return_intervals = df_proportions.copy()
+        df_proportions = pd.DataFrame(columns=["year", "landcover", "proportion"])
+        df_intervals = pd.DataFrame(columns=["year", "landcover", "interval"])
 
         region_name = region.get('name')
 
         burn_fn = f"data/nc/MODIS/MCD64A1/{region_name}/MCD64A1_500m.nc"
         burn_da = xr.open_dataset(burn_fn, mask_and_scale=False)["Burn_Date"]
 
-        landcover_filenames = glob.glob(f"data/tif/landcover/{region_name}/landcover*")
-        landcover_filenames = sorted(landcover_filenames)
+        landcover_folder = f"data/tif/landcover/{region_name}"
 
-        for padding, landcover_fn in zip(LANDCOVER_PADDINGS, landcover_filenames):
+        for year in np.unique(LANDCOVER_PERIODS):
 
+            landcover_fn = os.path.join(landcover_folder, f"landcover_{year}.tif")
             landcover_ds = gdal.Open(landcover_fn)
             landcover_arr = landcover_ds.ReadAsArray()
 
-            da = burn_da.sel(time=slice(*padding))
+            period = (
+                str(int(year) - LANDCOVER_PADDING),
+                str(int(year) + LANDCOVER_PADDING)
+            )
+            da = burn_da.sel(time=slice(*period))
             burn_mask = (da > 0).any(axis=0)
             burn_sum = (da > 0).sum(axis=0).values
 
@@ -54,18 +62,13 @@ if __name__ == "__main__":
                 landcover_burn_sum = burn_sum[mask]
 
                 proportion = burned_pixels / landcover_pixels
-                proportions.append(proportion)
+                df_proportions.loc[len(df_proportions)] = [year, name, proportion]
 
                 fri = np.mean((nyears + 1) / landcover_burn_sum)
-                return_intervals.append(fri)
-
-            df_proportions["_".join(padding)] = proportions
-            df_return_intervals["_".join(padding)] = return_intervals
+                df_intervals.loc[len(df_intervals)] = [year, name, fri]
 
         output_folder = f"results/csv/{region_name}"
         save_to_proportions = os.path.join(output_folder, "proportions_by_landcover.csv")
-        save_to_return_intervals = os.path.join(
-            output_folder, "return_intervals_by_landcover.csv"
-        )
+        save_to_intervals = os.path.join(output_folder, "intervals_by_landcover.csv")
         df_proportions.to_csv(save_to_proportions, index=False)
-        df_return_intervals.to_csv(save_to_return_intervals, index=False)
+        df_intervals.to_csv(save_to_intervals, index=False)
